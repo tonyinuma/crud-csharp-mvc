22b3237b845b1b7359bb48f56590bd1288001b2a
tonyinuma <tony.inuma@gmail.com>
2019-12-11 14:55:59 -0500

    Initial Commit C# Dev Course

---
+++ cmvc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace cmvc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ cmvc/Controllers/AccessController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cmvc.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(String user, String password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch(Exception ex)
+            {
+                return Content("Ocurrio un error :( "+ex.Message);
+            }
+            //return View();
+        }
+    }
+}
