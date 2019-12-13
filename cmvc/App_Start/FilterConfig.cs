b0578665bd1f9892c3a9f10697c76de8e864f154
tonyinuma <tony.inuma@gmail.com>
2019-12-13 16:18:15 -0500

    Feat: Sessions And  Filters

--- cmvc/App_Start/FilterConfig.cs
+++ cmvc/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace cmvc
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.VerifySession());
         }
     }
 }
---
+++ cmvc/Filters/VerifySession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using cmvc.Models;
+using cmvc.Controllers;
+
+namespace cmvc.Filters
+{
+    public class VerifySession : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var oUser = (user)HttpContext.Current.Session["User"];
+
+            if (oUser == null)
+            {
+                if (filterContext.Controller is AccessController == false )
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Access/Index");
+                }
+            }
+            else
+            {
+                if (filterContext.Controller is AccessController == true)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Home/Index");
+                }
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
