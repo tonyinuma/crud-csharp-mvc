3e35ab61bee6f43053b93b921f00be3c8c3c2af4
tonyinuma <tony.inuma@gmail.com>
2019-12-17 16:00:26 -0500

    Feat: Cerrar Sesion

---
+++ cmvc/Controllers/CerrarController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cmvc.Controllers
+{
+    public class CerrarController : Controller
+    {
+        // GET: Cerrar
+        public ActionResult logoff()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index","Access");
+        }
+
+    }
+}
