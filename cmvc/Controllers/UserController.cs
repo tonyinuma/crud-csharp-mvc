e4dd471d7d3f647b7e17df3e926d5d570d9f870d
tonyinuma <tony.inuma@gmail.com>
2019-12-17 15:48:28 -0500

    Feat: Edit and Deleted with Entity Framework

--- cmvc/Controllers/UserController.cs
+++ cmvc/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using cmvc.Models;
 using cmvc.Models.TableViewModels;
+using cmvc.Models.ViewModels;
 
 namespace cmvc.Controllers
 {
@@ -28,5 +29,108 @@ namespace cmvc.Controllers
             }
             return View(lst);
         }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(UserViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new cmvcEntities())
+            {
+                user oUser = new user();
+                oUser.idState = 1;
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+                oUser.password = model.Password;
+
+                db.user.Add(oUser);
+
+                db.SaveChanges();
+
+            }
+
+
+            return Redirect(Url.Content("~/User/"));
+
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            EditUserViewModel model = new EditUserViewModel();
+
+            using(var db =  new cmvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                model.Edad = (int)oUser.edad;
+                model.Email = oUser.email;
+                model.Id = oUser.id;
+            }
+
+                return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new cmvcEntities())
+            {
+                var oUser = db.user.Find(model.Id);
+                oUser.email = model.Email;
+                oUser.edad = model.Edad;
+
+                if (model.Password != null && model.Password.Trim() != "")
+                {
+                    oUser.password = model.Password;
+                }
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+            return Redirect(Url.Content("~/User/"));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+
+            using (var db = new cmvcEntities())
+            {
+                var oUser = db.user.Find(Id);
+                //1 active
+                //2 inactive
+                //3 deleted
+                oUser.idState = 3;
+
+                db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+
+            return Content("1");
+        }
+
+
     }
+
+
+
 }
---
+++ cmvc/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace cmvc.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 1)]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas deben ser iguales")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+
+        public int Id { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100, ErrorMessage = "El {0} debe tener al menos {1} caracteres", MinimumLength = 1)]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas deben ser iguales")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public int Edad { get; set; }
+    }
+}
